20fd60dfbcfb68f0afe7e31972ad02e362a15953
jrafaaael <jrmd070102@gmail.com>
2022-08-02 17:10:37 -0400

    send random data via serial

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,9 +1,8 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  delay(2000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println(String(random(100)));
 }
