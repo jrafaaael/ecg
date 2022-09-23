5fcb9586fb2f8682b13a9d35c0c005205cd07f1f
jrafaaael <jrmd070102@gmail.com>
2022-09-22 21:52:44 -0400

    implement kalman filter

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,10 +1,23 @@
+#include <SimpleKalmanFilter.h>
+
 #define ecg A0
+
+SimpleKalmanFilter simpleKalmanFilter(2, 2, 0.01);
+
+const long SERIAL_REFRESH_TIME = 10;
+long refresh_time;
 
 void setup() {
   Serial.begin(9600);
-  delay(2000);
 }
 
 void loop() {
-    Serial.println(analogRead(ecg));
+  unsigned int value = analogRead(ecg);
+  float estimated_value = simpleKalmanFilter.updateEstimate(value);
+
+  if (millis() > refresh_time) {
+    Serial.println(estimated_value, 4);
+
+    refresh_time = millis() + SERIAL_REFRESH_TIME;
+  }
 }
