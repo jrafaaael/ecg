1b11a3b2d003c5776deea23138b90896bf142ea2
jrafaaael <jrmd070102@gmail.com>
2022-09-25 10:53:25 -0400

    send data received from serial port through websocket

--- apps/server/src/server.js
+++ apps/server/src/server.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
 import parser from './serial.js';
 
 const PORT = 8080;
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
 
 app.get('/ping', (req, res) => {
   res.json({
@@ -16,7 +24,7 @@ parser.on('error', (err) => {
 
 parser.on('data', (chunk) => {
   const data = Number(chunk);
-  console.log(data);
+  io.emit('data', data);
 });
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`listening on port ${PORT}`));
