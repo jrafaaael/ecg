b1629df98d46f8b42c5831d8e68a590733be705a
jrafaaael <jrmd070102@gmail.com>
2022-10-01 21:38:25 -0400

    add pause button

--- apps/client/src/main.js
+++ apps/client/src/main.js
@@ -1,30 +1,36 @@
-import { io } from 'socket.io-client';
-import { Chart, registerables } from 'chart.js';
-import { CHART_OPTIONS, INITIAL_DATA } from './utils/chart';
-import './fullscreen';
+import { io } from "socket.io-client";
+import { Chart, registerables } from "chart.js";
+
+import { CHART_OPTIONS, INITIAL_DATA } from "./utils/chart";
+import "./fullscreen";
 
 Chart.register(...registerables);
 
 const socket = io(`${window.location.hostname}:8080`);
-const ctx = document.getElementById('myChart').getContext('2d');
-
+const ctx = document.getElementById("myChart").getContext("2d");
+const pause = document.getElementById("pause");
 const chart = new Chart(ctx, {
-  type: 'line',
+  type: "line",
   data: INITIAL_DATA,
   options: CHART_OPTIONS,
 });
+let isPaused = false;
 
-socket.on('data', (data) => {
-  const values = data.map(({ value }) => value);
-  const dates = data.map(({ date }) => date);
+pause.addEventListener("click", () => (isPaused = !isPaused));
 
-  chart.data.datasets[0].data.push(...values);
-  chart.data.labels.push(...dates);
+socket.on("data", (data) => {
+  if (!isPaused) {
+    const values = data.map(({ value }) => value);
+    const dates = data.map(({ date }) => date);
 
-  chart.update();
+    chart.data.datasets[0].data.push(...values);
+    chart.data.labels.push(...dates);
 
-  if (chart.data.datasets[0].data.length >= 50) {
-    chart.data.datasets[0].data.splice(0, 10);
-    chart.data.labels.splice(0, 10);
+    chart.update();
+
+    if (chart.data.datasets[0].data.length >= 100) {
+      chart.data.datasets[0].data.splice(0, 10);
+      chart.data.labels.splice(0, 10);
+    }
   }
 });
