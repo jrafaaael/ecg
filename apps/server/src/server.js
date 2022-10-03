944496dc7499c050cfb8523996cc30b266457015
jrafaaael <jrmd070102@gmail.com>
2022-10-03 10:40:21 -0400

    serve static frontend files

--- apps/server/src/server.js
+++ apps/server/src/server.js
@@ -1,18 +1,30 @@
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import parser from './serial.js';
 
 const PORT = 8080;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: "*",
   },
 });
 let values = [];
 let length = 0;
+
+app.use(
+  express.static(join(__dirname, "..", "..", "client", "dist"))
+);
+
+app.use((_req, res, _next) => {
+  res.sendFile(join(__dirname, "..", "..", "client", "dist", "index.html"));
+});
 
 app.get("/ping", (_, res) => {
   res.json({
